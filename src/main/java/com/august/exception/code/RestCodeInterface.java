7d1ef0cd57209fbc0ba4e3449de6c5b18e5f0770
august <august.org@aliyun.com>
2023-11-30 11:56:37 +0800

    delete all

--- src/main/java/com/august/exception/code/RestCodeInterface.java
+++
@@ -1,6 +0,0 @@
-package com.august.exception.code;
-
-public interface RestCodeInterface {
-    int getCode();
-    String getMsg();
-}
--- src/main/java/com/august/mapper/SysLogMapper.java
+++
@@ -1,9 +0,0 @@
-package com.august.mapper;
-
-import com.august.entity.SysLog;
-
-public interface SysLogMapper {
-    int insert(SysLog record);
-
-    int insertSelective(SysLog record);
-}
